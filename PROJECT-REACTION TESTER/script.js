ff2278d8cbc30fa9dd8c53fe57baaaed8e2be926
siddarthduttsharma <116385724+siddarthduttsharma@users.noreply.github.com>
2024-01-25 00:05:30 +0530

    Add files via upload

---
+++ PROJECT-REACTION TESTER/script.js
@@ -0,0 +1,35 @@
+var start = new Date().getTime();
+
+function move() {
+    var left = Math.random() * 300 + "px";
+    var top = Math.random() * 300 + "px";
+    var wh = (Math.random() * 400 + 100) + "px";
+    var color = getRandomColor();
+    
+    document.getElementById("shape").style.left = left;
+    document.getElementById("shape").style.top = top;
+    document.getElementById("shape").style.width = wh;
+    document.getElementById("shape").style.height = wh;
+    document.getElementById("shape").style.backgroundColor = color;
+    document.getElementById("shape").style.display = "block";
+    start = new Date().getTime();
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+move();
+
+document.getElementById("shape").onclick = function() {
+    document.getElementById("shape").style.display = "none";
+    var end = new Date().getTime();
+    var timeTaken = (end - start) / 1000;
+    alert(timeTaken);
+    move();
+}
